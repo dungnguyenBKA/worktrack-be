6b97d7ff95edb7ac7f75b07225d791bfb72f1de4
chavis <chavis@g.m>
2024-06-04 23:30:39 +0700

    update

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -3,54 +3,6 @@
 @section('content')
 <div class="container-fluid pb-3">
     <div class="card p-3 rounded-lg">
-        @roles('admin')
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="card rounded-lg">
-                        <div class="card-header card-title">{{ __('layouts.upload_user') }}</div>
-                        <div class="card-body">
-                            <form id="upload-user" action="{{ route("users.uploadUsers") }}" method="POST" enctype="multipart/form-data">
-                                @csrf
-                                <div class="form-group row">
-                                    <div class="col-xl-4">
-                                        <div class="custom-file @error('file-user') is-invalid @enderror" style="margin-top: 0.3rem">
-                                            <input type="file" class="file-user" name="file-user" id="file-user">
-                                            <label class="custom-file-label" for="file-user">{{ __('layouts.choose_file') }}</label>
-                                        </div>
-                                        @error('file-user')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                    </div>
-                                    <div class="col-xl-2">
-                                        <div class="form-check @error('type') is-invalid @enderror">
-                                            <input class="form-check-input" id="type-0" type="radio" name="type" checked="" value="0">
-                                            <label class="form-check-label" for="type-0">{{ __('layouts.do_not_overwrite_user') }}</label>
-                                        </div>
-                                        <div class="form-check">
-                                            <input class="form-check-input" id="type-1" type="radio" name="type" value="1">
-                                            <label class="form-check-label" for="type-1">{{ __('layouts.overwrite_user') }}</label>
-                                        </div>
-                                        @error('type')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                    </div>
-                                    <div class="col-xl-3"><button type="submit" id="btn-upload-user" class="btn btn-primary" style="margin-top: 0.3rem">{{ __('layouts.upload') }}</button></div>
-                                </div>
-                                <div class="form-group row">
-                                    <div class="col-md-12">
-                                        <a href="{{ asset('/files/Template_Import_Users.csv') }}" download>{{ __('layouts.download_template_import') }}</a>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        @endroles
         <div class="row">
             <div class="col-md-8">
                 @roles('admin')

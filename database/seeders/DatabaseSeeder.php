c504ce1bef62d05127098c57a734577788ecf177
chavis <chavis@g.m>
2024-06-04 23:28:28 +0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,12 +19,23 @@ class DatabaseSeeder extends Seeder
             MasterSeeder::class,
         ]);
         User::create([
-            'staff_id' => '20180000',
+            'staff_id' => '1',
+            'first_name' => 'Admin',
+            'last_name' => 'BKA',
+            'email' => 'admin@bka.vn',
+            'position_id' => 1,
+            'role' => config('common.user.role.admin'),
+            'password' => bcrypt('12345678'),
+            'created_by' => 1,
+        ]);
+
+        User::create([
+            'staff_id' => '2',
             'first_name' => 'Nguyen Minh',
             'last_name' => 'Dung',
             'email' => 'dung.nm184079@bka.vn',
             'position_id' => 1,
-            'role' => config('common.user.role.admin'),
+            'role' => config('common.user.role.user'),
             'password' => bcrypt('12345678'),
             'created_by' => 1,
         ]);
